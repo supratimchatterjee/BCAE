30fa7531c4ccf99db6252f54619d345da4a0445f
supratimchatterjee <36877257+supratimchatterjee@users.noreply.github.com>
2018-03-18 19:36:02 +0530

    BCAE
    Final Commit

---
+++ enfold-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+* Add your own functions here. You can also copy some of the theme functions into this file. 
+* Wordpress will use those functions instead of the original functions then.
+*/
+
+add_theme_support('avia_template_builder_custom_css');
